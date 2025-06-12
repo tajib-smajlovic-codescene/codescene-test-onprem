73d4674608bf2c5fc4ce60bd6199f1fee89746fb
tajib-smajlovic-codescene <tajib.smajlovic@codescene.com>
2025-06-12 10:46:36 +0200

    #26

--- main.js
+++ main.js
@@ -3,3 +3,4 @@ console.log('World')
 console.log('This is a test file');
 console.log('This is another test file');
 console.log('This is yet another test file');
+console.log('This is a test file');
